44f372ad7213d582d4738c1f807380389c23977a
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-05 17:03:48 -0600

    work in progress branch

--- database-spike.js
+++ database-spike.js
@@ -2,8 +2,20 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-database.raw('SELECT * FROM foods, meals')
-.then( function(data) {
+// database.raw('SELECT * FROM foods, meals')
+// .then( function(data) {
+//   console.log(data.rows)
+//   process.exit();
+// });
+
+database.raw(
+  `SELECT *.foods,
+  name.meals as meal 
+  FROM food_meals, meals, foods
+  WHERE id.meals = meal_id.food_meals
+  AND id.foods = food_id.food_meals
+  GROUP BY meal`
+  ).then( function(data) {
   console.log(data.rows)
   process.exit();
-});
+})
