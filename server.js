7e2b23bb08b32cb4f916e0dd16a8873fbc0f6ef6
Jack Pincus <jwpincus@gmail.com>
2017-07-07 18:20:38 -0600

    fix cors issue

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Quantified Self API'
 
